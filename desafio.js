b68b32512fba09520c531918752fd3ddf42a3112
PlayMa256 <matheus.g.silva@hotmail.com>
2016-02-27 00:42:45 -0300

    comitando com codigo comentado para guarda-lo

--- desafio.js
+++ desafio.js
@@ -1,5 +1,8 @@
 // Imports
 var fs = require('fs');
+var _ = require('lodash');
+var extend = require('util')._extend;
+var R = require('ramda');
 var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance(),
     PNF = require('google-libphonenumber').PhoneNumberFormat,
     PNT = require('google-libphonenumber').PhoneNumberType;
@@ -86,8 +89,11 @@ function mergeEVERYTHING(contentA) {
         currentElement = elemento;
         if (oldElement !== undefined) {
             if (currentElement["fullname"] === oldElement["fullname"]) {
+                // console.log("CURRENT ELEMENT: "+ JSON.stringify(currentElement, null, 4));
+                // console.log("OLD ELEMENT : "+ JSON.stringify(oldElement, null, 4));
                 var tempObject = mergeObjects(currentElement, oldElement);
-                mergedJSON.push(tempObject);
+                console.log(JSON.stringify(tempObject, null, 4));
+                //mergedJSON.push(tempObject);
             } else {
                 mergedJSON.push(elemento);
             }
@@ -98,11 +104,8 @@ function mergeEVERYTHING(contentA) {
 
     return mergedJSON;
 }
-
-function isArray(o) {
-    return Object.prototype.toString.call(o) == "[object Array]";
-}
-
+// Assumes that target and source are either objects (Object or Array) or undefined
+// Since will be used to convert to JSON, just reference objects where possible
 function mergeObjects(target, source) {
     var item, tItem, o, idx;
     if (typeof source == 'undefined') {
@@ -110,49 +113,129 @@ function mergeObjects(target, source) {
     } else if (typeof target == 'undefined') {
         return target;
     }
+
     for (var prop in source) {
         item = source[prop];
+        //        console.log("ITEM:> "+ JSON.stringify(item, null, 4));
+        //check if the first element is indeed an object.
         if (typeof item == 'object' && item !== null) {
-            if (isArray(item) && item.length) {
+            //if the first item is an array
+            if (_.isArray(item) && item.length) {
+                //dealing with array of primitives
+                //console.log("SOU UM ARRAY");
                 if (typeof item[0] != 'object') {
-                    tItem = target[prop];
-                    if (!tItem) {
-                        target[prop] = item;
-                    } else {
-                        o = {};
-                        for (var i = 0, iLen = tItem.length; i < iLen; i++) {
-                            o[tItem[i]] = true
-                        }
-                        for (var j = 0, jLen = item.length; j < jLen; j++) {
-                            if (!(item[j] in o)) {
-                                tItem.push(item[j]);
-                            }
-                        }
-                    }
+                    // console.log("SOU UM ARRAY de primitivas (string, etc...)");
+                    item.forEach(function(conteudo) {
+                        //push to the target all the elements;
+                        target[prop].push(conteudo);
+                    });
                 } else {
-                    idx = {};
-                    tItem = target[prop]
-                    for (var k = 0, kLen = tItem.length; k < kLen; k++) {
-                        idx[tItem[k].id] = tItem[k];
-                    }
-                    for (var l = 0, ll = item.length; l < ll; l++) {
-                        if (!(item[l].id in idx)) {
-                            tItem.push(item[l]);
+                    //console.log("SOU UM ARRAY de objetos");
+                    //dealing with array of objects;
+                    var targetArray = [];
+                    var sourceArray = [];
+                    for (i = 0; i < item.length; i++) {
+                        idx = {};
+                        //    console.log("ITEM: " + JSON.stringify(item[i], null, 4) + " - TARGET " + JSON.stringify(target[prop][i], null, 4));
+                        if (_.isEqual(item[i], target[prop][i])) {
+                            targetArray.push(item[i]);
                         } else {
-                            mergeObjects(idx[item[l].id], item[l]);
+                            targetArray.push(item[i]);
+                            targetArray.push(target[prop][i]);
                         }
+                        // for(var attr in item[i]){
+                        //     tItem = target[prop][i][attr];
+                        //     var sourceItem = source[prop][i][attr];
+                        //     console.log("TITEM AT ["+prop+"]["+i+"]["+attr+"] ===>" + require('util').inspect(tItem, {showHidden:true, depth:null}));
+                        //     console.log("SOURCE ITEM AT ["+prop+"]["+i+"]["+attr+"] ===> " + require('util').inspect(sourceItem, {showHidden:true, depth:null}));
+                        //     if(_.isArray(tItem) && _.isArray(source[attr])){
+                        //         console.log("AMBOS SOMOS ARRAYS");
+                        //         tItem.concat(source[attr]);
+                        //         idx[attr] = tItem;
+                        //     }else if(_.isArray(tItem) && !_.isArray(sourceItem)){
+                        //         console.log("APENAS O OLD ELEMENT NAO É ARRAY");
+                        //         tItem.push(source[attr]);
+                        //         idx[attr] = tItem;
+                        //     }else if(typeof tItem !== "object" && typeof sourceItem !== "object" && tItem !== sourceItem){
+                        //         console.log("ESTES AQUI SAO O QUE SAO DIFERENTES: " + tItem + " E : " + sourceItem);
+                        //         var idx2 = extend({}, idx);
+                        //         console.log("ESTE é O IDX2 " + JSON.stringify(idx2, null, 4));
+                        //         idx2[attr] = sourceItem;
+                        //         console.log("ESTE é O IDX2 DEPOIS DE ALTERADO O ENDERECO" + JSON.stringify(idx2, null, 4));
+                        //         targetArray.push(idx2);
+                        //
+                        //
+                        //         idx[attr] = tItem;
+                        //     }
+                        //     else{
+                        //         console.log("ESTOU SOU EU CASO NAO SEJA NADA");
+                        //         idx[attr] = tItem;
+                        //     }
+                        //     console.log("ESTOU SOU O NOVO EU --->" + JSON.stringify(idx, null, 4));
+                        //
+                        // }
+                        // targetArray.push(idx);
+                        // console.log("ESTE AQUI É PRA ONDE ESTÃO INDO TODAS AS COISAS " + JSON.stringify(targetArray, null, 4));
+
                     }
+                    //console.log(JSON.stringify(targetArray, null, 4));
+                    target[prop] = targetArray;
                 }
             } else {
+                //if its a normal object
                 mergeObjects(target[prop], item);
             }
 
-        } else {
-            target[prop] = item;
+        } else if(prop.startsWith("invisible") || prop.startsWith("see_all")) {
+            // item is a primitive, just copy it over
+            var resultOfSum = !!(target[prop]+item);
+            target[prop] = resultOfSum;
         }
     }
     return target;
 }
+
+// function mergeObjects(target, source){
+//     var item, tItem, o, idx;
+//     if (typeof source == 'undefined') {
+//       return source;
+//     } else if (typeof target == 'undefined') {
+//       return target;
+//     }
+//
+//     for (var prop in source) {
+//         item = source[prop];
+//         //check if the first element is indeed an object.
+//         if (typeof item == 'object' && item !== null) {
+//             //if the first item is an array
+//           if (_.isArray(item) && item.length) {
+//               //dealing with array of primitives
+//               if (typeof item[0] != 'object') {
+//                   item.forEach(function(conteudo){
+//                       //push to the target all the elements;
+//                       target[prop].push(conteudo);
+//                   });
+//               }else{
+//                 //dealing with array of objects;
+//                 for(var attr in item){
+//                     idx = {};
+//                     tItem = target[attr]
+//                     mergeObjects(tItem,item);
+//                 }
+//               }
+//           }//if its a normal object
+//           else {
+//            // deal with object
+//            mergeObjects(target[prop],item);
+//          }
+//
+//         } else {
+//              // item is a primitive, just copy it over
+//              target[prop] = item;
+//            }
+//     }
+//     return target;
+// }
 // End Functions
 var f = fs.readFileSync('./alunos.csv', {
     encoding: 'utf-8'
@@ -202,9 +285,10 @@ f.forEach(function(entry) {
     }
     finalJSON.push(temp);
 });
+//console.log(JSON.stringify(finalJSON, null, 4));
 finalJSON = mergeEVERYTHING(finalJSON);
-fs.writeFile("./resultado.json", JSON.stringify(finalJSON, null, 4), function(error) {
-    if (error) {
-        throw error;
-    }
-});
+// fs.writeFile("./resultado.json", JSON.stringify(finalJSON, null, 4), function(error) {
+//     if (error) {
+//         throw error;
+//     }
+// });

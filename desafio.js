b0c6bf35f141ed738da1f220d48fbf6b64555220
PlayMa256 <matheus.g.silva@hotmail.com>
2016-03-01 15:35:51 -0300

    arrumando alguns bugs e finalizado

--- desafio.js
+++ desafio.js
@@ -1,6 +1,7 @@
 // Imports
 var fs = require('fs');
 var _ = require('lodash');
+var R = require('ramda');
 var validator = require('validator');
 var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance(),
     PNF = require('google-libphonenumber').PhoneNumberFormat,
@@ -21,7 +22,7 @@ function trataClasses(conteudo) {
                 classes.push(classe.trim().replace(/\n/g, ""));
             });
         } else {
-            classes.push(conteudo);
+            return conteudo;
         }
     }
     return classes;
@@ -85,43 +86,49 @@ function fillOthers(conteudo, header) {
     }
 }
 
-function concatObjectsAndArrays(target, source) {
-    var item, tItem;
-    if (typeof source == 'undefined') {
-        return source;
-    } else if (typeof target == 'undefined') {
-        return target;
-    }
-
-    for (var prop in source) {
-        item = source[prop];
-        //check if the first element is indeed an object.
-        if (typeof item == 'object' && item !== null) {
-            //if the first item is an array
-            if (_.isArray(item) && item.length) {
-                //dealing with array of primitives
-                if (typeof item[0] != 'object') {
-                    item.forEach(function(conteudo) {
-                        //push to the target all the elements;
-                        target[prop].push(conteudo);
-                    });
-                } else {
-                    var targetArray = item.concat(target[prop]);
-                    target[prop] = targetArray;
-                }
-            } else {
-                //if its a normal object
-                concatObjectsAndArrays(target[prop], item);
-            }
-
-        } else if (prop.startsWith("invisible") || prop.startsWith("see_all")) {
-            // item is a primitive, just copy it over
-            var resultOfSum = !!(target[prop] + item);
-            target[prop] = resultOfSum;
-        }
-    }
-    return target;
-}
+/*
+* This function is not used. However, this is another implementation of the line 167, but there
+* the implementation is way more cleaner and has a better performance.
+* The usage of the library Ramda fits perfectly for this scope of usage, can not be useful in future problems.
+* You can remove this uncommented lines if you want.
+*/
+// function concatObjectsAndArrays(target, source) {
+//     var item, tItem;
+//     if (typeof source == 'undefined') {
+//         return source;
+//     } else if (typeof target == 'undefined') {
+//         return target;
+//     }
+//
+//     for (var prop in source) {
+//         item = source[prop];
+//         //check if the first element is indeed an object.
+//         if (typeof item == 'object' && item !== null) {
+//             //if the first item is an array
+//             if (_.isArray(item) && item.length) {
+//                 //dealing with array of primitives
+//                 if (typeof item[0] != 'object') {
+//                     item.forEach(function(conteudo) {
+//                         //push to the target all the elements;
+//                         target[prop].push(conteudo);
+//                     });
+//                 } else {
+//                     var targetArray = item.concat(target[prop]);
+//                     target[prop] = targetArray;
+//                 }
+//             } else {
+//                 //if its a normal object
+//                 concatObjectsAndArrays(target[prop], item);
+//             }
+//
+//         } else if (prop.startsWith("invisible") || prop.startsWith("see_all")) {
+//             // item is a primitive, just copy it over
+//             var resultOfSum = !!(target[prop] + item);
+//             target[prop] = resultOfSum;
+//         }
+//     }
+//     return target;
+// }
 
 function mergeAttrs(array) {
     var resultArray = [];
@@ -152,11 +159,17 @@ function mergeAttrs(array) {
 function mergeObjectsWithSameElement(contentA) {
     var mergedJSON = [];
     var currentElement, oldElement;
+    var dupeMergeFn = function(a, b) {
+        return (Array.isArray(a)) ? a.concat(b) : b;
+    };
     contentA.forEach(function(elemento) {
         currentElement = elemento;
         if (oldElement !== undefined) {
             if (currentElement["fullname"] === oldElement["fullname"]) {
-                var tempObject = concatObjectsAndArrays(currentElement, oldElement);
+            //var tempObject = concatObjectsAndArrays(currentElement, oldElement);
+                var tempObject = R.mergeWith(dupeMergeFn, currentElement, oldElement);
+                tempObject["see_all"] = !!(currentElement["see_all"]+oldElement["see_all"]);
+                tempObject["invisible"] = !!(currentElement["invisible"]+oldElement["invisible"]);
                 mergedJSON.push(tempObject);
             } else {
                 var object = currentElement;
@@ -204,13 +217,20 @@ f.forEach(function(entry) {
                 temp[headers[i]] = tempLine;
             } else if (headers[i].startsWith("class")) {
                 classes = trataClasses(tempLine);
-                if (classes.length > 0) {
-                    classes.forEach(function(classe) {
-                        temp["classes"].push(classe);
-                    });
-                } else {
-                    delete temp["classes"];
-                    temp["classes"] = classes[0];
+                if(_.isArray(classes)){
+                    if(_.isArray(temp["classes"])){
+                        classes.forEach(function(classe){
+                            temp["classes"].push(classe);
+                        });
+                    }else{
+                        temp["classes"] = classes;
+                    }
+                }else{
+                    if(_.isArray(temp["classes"])){
+                        temp["classes"].push(classes);
+                    }else{
+                        temp["classes"] = classes;
+                    }
                 }
             } else if (headers[i].indexOf("invisible") >= 0 || headers[i].indexOf("see_all") >= 0) {
                 others = fillOthers(tempLine);
@@ -225,7 +245,6 @@ f.forEach(function(entry) {
     }
     finalJSON.push(temp);
 });
-
 finalJSON = mergeObjectsWithSameElement(finalJSON);
 fs.writeFile("./resultado.json", JSON.stringify(finalJSON, null, 4), function(error) {
     if (error) {
